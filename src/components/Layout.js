88214986376b8eb787b1e50956838fc0b5180097
ArcticJunkie <pikka99@gmail.com>
2021-08-31 18:08:41 +0530

    default dark bg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 
 function Layout(props) {
     return (
-        <div className="text-warm font-inter text-lg xl:text-2xl overflow-hidden">
+        <div className="text-warm bg-dark font-inter text-lg xl:text-2xl overflow-hidden">
             <div className="px-6 xl:px-40 bg-grain bg-contain">
                 {props.children}
             </div>
